56d57a84724df021d1cc3b47b5024f66c73f3755
miwanoff <marina.iva.noffa@gmail.com>
2023-05-07 12:53:36 +0300

    Add Bootstrap and useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-function App() {
+import React, { useState } from "react";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./assets/images/logo.png";
+
+const api = {
+  key: "45320eb98dd32e71513cf76378fc81e7",
+  base: "http://api.openweathermap.org/data/2.5/",
+  icon: "https://openweathermap.org/img/w",
+};
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
   return (
-    <div className="App">
-      Hello
+    <div className="container">
+      <Header className="alert alert-warning mt-4 p-5 rounded text-center" />
+      <div className="row">
+        <div className="search-panel col-sm-12 col-md-4 my-3">
+          <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Search"
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
+const Image = (props) => {
+  return <img src={props.src} alt="logo" style={{ width: "100px" }} />;
+};
+
+const Header = (props) => {
+  return (
+    <div className={props.className}>
+      <Image src={logo} />
+      <h1>Weather</h1>
+    </div>
+  );
+};
 
 export default App;
